e94e9fe0796cea94e5f41a2066bca94da11671ad
lijun30 <lijun30@meituan.com>
2017-03-08 14:21:24 +0800

    6

--- 6_MAKE_IT_MODULAR_ONE.js
+++ 6_MAKE_IT_MODULAR_ONE.js
@@ -2,10 +2,10 @@ module.exports = (dir, ext, callback) => {
   require('fs').readdir(
     dir, (err, list) => {
       if (err) return callback(err);
-      list
-        .filter(file => require('path').extname(file) === '.' + ext)
-        .forEach(filterdFile => console.log(filterdFile))
-      callback(null, list);
+      callback(null,
+        list
+          .filter(file => require('path').extname(file) === '.' + ext)
+      )
     }
   )
 }
