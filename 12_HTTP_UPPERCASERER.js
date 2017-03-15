41143bad8ff5bc0788164a5ff6926877d5013240
lijun30 <lijun30@meituan.com>
2017-03-15 15:52:31 +0800

    12

--- 12_HTTP_UPPERCASERER.js
+++ 12_HTTP_UPPERCASERER.js
@@ -2,9 +2,7 @@ require('http').createServer((request, response) => {
   if (request.method !== 'POST') {
     return;
   }
-
   request.pipe(
     require('through2-map')(chunk => chunk.toString().toUpperCase())
   ).pipe(response)
 }).listen(process.argv[2])
-
